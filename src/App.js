7eb32911b6a59c083c2a625cc239b98e78f1b74e
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-03 09:10:12 -0700

    added fetch requests to Json API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import store from './dummy-store';
+// import store from './dummy-store';
 import {Route} from 'react-router-dom'
 import Main from './components/main';
 import MainSideBar from './components/mainsidebar'
@@ -12,15 +12,51 @@ import StoreContext from './context/StoreContext'
 class App extends React.Component {
   
   state = {
-    store
+    folders: [],
+    notes: []
+  }
+
+  componentDidMount(){
+    //fetch request
+    fetch('http://localhost:9090/folders')
+      .then(res => {
+        if(res.ok) {
+          return res.json();
+        }
+        throw new Error(res.statusText);
+      })
+      .then(resJson => {
+        this.setState({
+          folders: resJson
+        })
+      }
+      )
+      .catch(error => {
+        console.log(error);
+      })
+    fetch('http://localhost:9090/notes')
+      .then(res => {
+        if(res.ok) {
+          return res.json();
+        }
+        throw new Error(res.statusText);
+      })
+      .then(resJson => {
+        this.setState({
+          notes: resJson
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   render(){
 
   return (
     <StoreContext.Provider value = {{
-      folders: this.state.store.folders,
-      notes: this.state.store.notes
+      folders: this.state.folders,
+      notes: this.state.notes
     }}>
     <div className="App">
       <Header/>
